97c02281abb767cfb10099a4c9893638d8cf04dc
Pablo Varela <pablovarela182@gmail.com>
2017-11-03 15:51:48 +0100

    Mejorar los tests

--- e2e/anuncios.e2e.js
+++ e2e/anuncios.e2e.js
@@ -44,36 +44,31 @@ test('Retorna una lista de anuncios', async t => {
 })
 
 test('Inserta un anuncio nuevo', async t => {
-  const res = await agent
-    .post(apiUrl)
-    .send({ nombre: 'Nuevo anuncio' })
+  const res = await agent.post(apiUrl).send({ nombre: 'Nuevo anuncio' })
   t.is(res.body.nombre, 'Nuevo anuncio')
 })
 
 test('Borra un anuncio', async t => {
-  const res = await agent
-    .post(apiUrl)
-    .send({ nombre: 'Anuncio a borrar' })
-  await agent
-    .delete(`${apiUrl}/${res.body._id}`)
-    .then(() => t.pass())
+  const res = await agent.post(apiUrl).send({ nombre: 'Anuncio a borrar' })
+  await agent.delete(`${apiUrl}/${res.body._id}`).then(() => t.pass())
+  const { body } = await agent.get(apiUrl)
+  for (const anuncio of body) {
+    if (anuncio.nombre === 'Anuncio a borrar') {
+      t.fail()
+    }
+  }
+  t.pass()
 })
 
 test('Lanza un error al borrar un anuncio desconocido', async t => {
-  await agent
-    .delete(`${apiUrl}/foo`)
-    .then(({ statusCode }) => {
-      t.is(statusCode, 500)
-    })
+  const res = await agent.delete(`${apiUrl}/foo`)
+  t.is(res.statusCode, 500)
 })
 
 test('Modifica un anuncio existente', async t => {
   const resPlain = await agent.get(apiUrl)
   const anuncio = resPlain.body[0]
-  await agent
-    .put(`${apiUrl}/${anuncio._id}`)
-    .send({ nombre: 'Modificado' })
-  const resModified = await agent
-    .get(apiUrl)
+  await agent.put(`${apiUrl}/${anuncio._id}`).send({ nombre: 'Modificado' })
+  const resModified = await agent.get(apiUrl)
   t.is(resModified.body[0].nombre, 'Modificado')
 })
